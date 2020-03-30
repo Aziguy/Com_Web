bc7aae24a5b07bc5ae01d97daab1747fdb3920dc
Aziguy <hippolyte-kengni@etud.univ-tln.fr>
2020-03-30 23:40:33 +0200

    Utilisation de pandas, ajout des fonctions splitage() et adresseSite()

--- Principal.py
+++ Principal.py
@@ -17,21 +17,31 @@ exporter = utils.exportCsvAhref('www.portcrosparcnational.fr-backlinks-subdomain
 # =============================================================================
 
 dico = utils.mesDictionnaires('www.portcrosparcnational.fr-backlinks-subdomains.csv')
-print(dico[0])  # Attributs
-print(dico[1])  # Liens
-print(dico[2])  # Noeuds
+attribut = dico[0]  # Attributs
+liens = dico[1]  # Liens
+noeuds = dico[2]  # Noeuds
 
-liens = dico[1]
-noeuds = dico[2]
+newDict = dict()
+# for cle in attribut:
+#     newDict = attribut[cle]
+#     # if cle[0] == attribut[0]:
+#     #     newDict['Occurrences'] = valeur
+#     print(cle,":",attribut[cle])
+
+
+print(attribut)
+print(liens)
+print(noeuds)
+
 G = nx.DiGraph()
 
 for cle, valeur in liens.items():
     G.add_node(cle[0])
     G.add_edge(cle[0], cle[1], weight=valeur)
 
-print(G.nodes())
-print(G.edges())
-nx.write_gexf(G, "DomaineAggregation.gexf")
+# print(G.nodes())
+# print(G.edges())
+# nx.write_gexf(G, "DomaineAggregation.gexf")
 # Pour visualiser avec matplotlib.pyplot
 # nx.draw(G, with_labels=True)
 # plt.show()
